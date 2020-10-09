5727e60b38de168067dbf25329ed4be3fb0b6180
William Cruz <cruzentwickler@gmail.com>
2020-10-09 15:19:54 +0200

    Sending a Post request

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,6 +3,7 @@ import Burger from '../../components/Burger/Burger'
 import BuildControls from '../../components/Burger/BuildControls/BuildControls'
 import Modal from '../../components/UI/Modal/Modal'
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary'
+import axios from '../../axios-order'
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -75,7 +76,30 @@ class BurgerBuilder extends Component {
   }
 
   purchaseContinueHandler = () => {
-    alert('You continue')
+    // alert('You continue')
+    const order = {
+      ingredients: this.state.ingredients,
+      price: this.state.totalPrice,
+      customer: {
+        name: 'Willim Cruz',
+        address: {
+          street: 'Missionsstrasse 61',
+          zipCode: '4055',
+          country: 'Switzerland',
+        },
+        email: 'test@test.com',
+      },
+      deleveryMethod: 'fastest',
+    }
+
+    axios
+      .post('/orders.json', order)
+      .then((response) => {
+        console.log(response)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
   }
 
   render() {
