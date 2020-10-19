d460cd8fc3a6a4298435c4971fc6f367760ca8ed
William Cruz <cruzentwickler@gmail.com>
2020-10-19 12:58:30 +0200

    Installing Redux and React Redux

--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ import './index.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
 import { BrowserRouter } from 'react-router-dom'
+// Redux
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from './store/reducer'
+
+const store = createStore(reducer)
 
 ReactDOM.render(
   <React.StrictMode>
