54c2b8dc1dcbcdf5e2fbb266543b44979857b6c3
William Cruz <cruzentwickler@gmail.com>
2020-10-29 14:25:25 +0100

    Adding a Logout Link

---
+++ src/containers/Auth/Logout/Logout.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+// * Router
+import { Redirect } from 'react-router-dom'
+// * Redux
+import { connect } from 'react-redux'
+import * as actions from '../../../store/actions/index'
+
+class Logout extends Component {
+  componentDidMount() {
+    this.props.onLogout()
+  }
+
+  render() {
+    return <Redirect to="/" />
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onLogout: () => dispatch(actions.logout()),
+  }
+}
+
+export default connect(null, mapDispatchToProps)(Logout)
