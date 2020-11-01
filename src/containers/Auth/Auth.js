fee616322e3ef79e56e8ca120ef25c98ba1ecb2f
William Cruz <cruzentwickler@gmail.com>
2020-11-01 15:17:58 +0100

    Sharing the Validation Method

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { updatedObject } from '../../shared/utility'
+import { updatedObject, checkValidity } from '../../shared/utility'
 // * Router
 import { Redirect } from 'react-router-dom'
 // * Redux
@@ -53,43 +53,11 @@ class Auth extends Component {
     }
   }
 
-  checkValidity(value, rules) {
-    let isValid = true
-
-    if (!rules) {
-      return true
-    }
-
-    if (rules.required) {
-      isValid = value.trim() !== '' && isValid
-    }
-
-    if (rules.minLength) {
-      isValid = value.length >= rules.minLength && isValid
-    }
-
-    if (rules.maxLength) {
-      isValid = value.length <= rules.maxLength && isValid
-    }
-
-    if (rules.isEmail) {
-      const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
-      isValid = pattern.test(value) && isValid
-    }
-
-    if (rules.isNumeric) {
-      const pattern = /^\d+$/
-      isValid = pattern.test(value) && isValid
-    }
-
-    return isValid
-  }
-
   inputChangedHandler = (event, controlName) => {
     const updatedControls = updatedObject(this.state.controls, {
       [controlName]: updatedObject(this.state.controls[controlName], {
         value: event.target.value,
-        valid: this.checkValidity(
+        valid: checkValidity(
           event.target.value,
           this.state.controls[controlName].validation
         ),
