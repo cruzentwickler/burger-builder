0c7e414264cc5f6df99d5684ff5cffd11badfc34
William Cruz <cruzentwickler@gmail.com>
2020-11-03 15:23:15 +0100

    Using React.memo and more

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,36 +1,32 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Button from '../../UI/Button/Button'
 
-class OrderSummary extends Component {
-  render() {
-    const ingredientSummary = Object.keys(this.props.ingredients).map(
-      (igKey) => {
-        return (
-          <li key={igKey}>
-            <span style={{ textTransform: 'capitalize' }}>{igKey}</span>:{' '}
-            {this.props.ingredients[igKey]}
-          </li>
-        )
-      }
-    )
+const OrderSummary = (props) => {
+  const ingredientSummary = Object.keys(props.ingredients).map((igKey) => {
     return (
-      <>
-        <h3>Your Order</h3>
-        <p>A deliciuos burger with the following ingredients:</p>
-        <ul>{ingredientSummary}</ul>
-        <p>Continue to Checkout?</p>
-        <p>
-          <strong>Total Price: {this.props.price.toFixed(2)}</strong>{' '}
-        </p>
-        <Button clicked={this.props.purchaseCancelled} btnType="Danger">
-          CANCEL
-        </Button>
-        <Button btnType="Success" clicked={this.props.purchaseContinued}>
-          CONTINUE
-        </Button>
-      </>
+      <li key={igKey}>
+        <span style={{ textTransform: 'capitalize' }}>{igKey}</span>:{' '}
+        {props.ingredients[igKey]}
+      </li>
     )
-  }
+  })
+  return (
+    <>
+      <h3>Your Order</h3>
+      <p>A deliciuos burger with the following ingredients:</p>
+      <ul>{ingredientSummary}</ul>
+      <p>Continue to Checkout?</p>
+      <p>
+        <strong>Total Price: {props.price.toFixed(2)}</strong>{' '}
+      </p>
+      <Button clicked={props.purchaseCancelled} btnType="Danger">
+        CANCEL
+      </Button>
+      <Button btnType="Success" clicked={props.purchaseContinued}>
+        CONTINUE
+      </Button>
+    </>
+  )
 }
 
 export default OrderSummary
